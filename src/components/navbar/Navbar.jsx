1c7dbb76fcc4693443cd60cb4e71c4f32ebc6dd4
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-14 11:30:17 +0100

    ✨feat(navbar): création d'une navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import './navbar.scss'
+import React from 'react'
+import { ArrowDropDown, Notifications, Search } from '@material-ui/icons'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="container">
+        <div className="left">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="" />
+          <span>Accueil</span>
+          <span>Series</span>
+          <span>Films</span>
+          <span>Nouveautés</span>
+          <span>Favoris</span>
+        </div>
+        <div className="right">
+          <Search className='icon' />
+          <span>Enfants</span>
+          <Notifications className='icon' />
+          <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+
+          <div className="profile">
+            <ArrowDropDown className='icon' />
+            <div className="options">
+              <span>Paramètres</span>
+              <span>Déconnexion</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
